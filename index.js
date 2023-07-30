e2b1e0b41b776cfe5d3ae03e82a4cb633ad46d46
Do Le Long An <85084360+LaansDole@users.noreply.github.com>
2023-07-31 00:14:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const axios = require('axios');
 const fs = require('fs');
 
 axios.get("https://backend-omega-seven.vercel.app/api/getjoke")
-.then(res => {
-  const data = res.data;
-  const question = data[0].question;
-  const punchline = data[0].punchline;
+  .then(res => {
+    const data = res.data;
+    const question = data[0].question;
+    const punchline = data[0].punchline;
 
-  var text = `
+    var text = `
 # Daily Dev Joke
 
-- Fetches a joke from an API everyday and edits this README`
-  text += "\n\n**" + question + "**"
-  text += "\n*" + punchline + "*"
+`
+    text += "\n\n**" + question + "**"
+    text += "\n*" + punchline + "*"
 
-  fs.writeFile('README.md', text, 'utf-8', function(err, data) {
+    fs.readFile('README.md', 'utf-8', function (err, content) {
       if (err) throw err;
-      console.log('Done!');
+
+      const startIndex = content.indexOf('<div id="daily-joke">');
+      const endIndex = content.indexOf('</div>', startIndex) + 6;
+      const updatedContent = content.slice(0, startIndex) + '<div id="daily-joke">' + text + '</div>' + content.slice(endIndex);
+
+      fs.writeFile('README.md', updatedContent, 'utf-8', function (err) {
+        if (err) throw err;
+        console.log('Done!');
+      });
+    });
   })
-})
-.catch(err => {
-  console.log('Error: ', err.message);
-});
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
