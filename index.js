2066a64c5ca2c61fde65d681263383fb28b010ec
Do Le Long An <85084360+LaansDole@users.noreply.github.com>
2024-01-24 12:08:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ align="center" >
 
 ## 🤖 Daily Dev Joke 
     `;
-    text += "\n\n**" + question + "**";
-    text += "\n*" + punchline + "*";
+    text += "\n\n**<h3>" + question + "</h3>**"
+    text += "\n*" + punchline + "*"
 
     fs.writeFile('README.md', text, 'utf-8', function (err, data) {
       if (err) throw err;
