9dc775b8b2bbd347bdcdd110538088a1c03bd264
LaansDole <LaansDole@users.noreply.github.com>
2025-02-06 17:28:53 +0000

    Update site

--- script/cli/main-cli.js
+++ script/cli/main-cli.js
@@ -1,56 +1,19 @@
-const linkedin = "https://www.linkedin.com/in/do-le-long-an/";
-const github = "https://github.com/LaansDole";
-const email = "mailto:dolelongan@gmail.com";
-const game = "https://laansdole.github.io/random-dungeon-game/"
-
-const help = [
-  "<li> Press <kbd>tab</kbd> for auto-completion and use up and down arrows for previous commands.</li>",
-  "<br>",
-  "<li>aboutme        - Get to know me! <i class='fa-solid fa-user'></i></li>",
-  "<li>projects       - See the latest projects I've built <i class='fa-solid fa-folder'></i></li>",
-  "<li>linkedin       - Check out my LinkedIn <i class='fa-brands fa-linkedin'></i></li>",
-  "<li>github         - My GitHub profile <i class='fa-brands fa-github'></i></li>",
-  "<li>email          - Shoot me an email <i class='fa-solid fa-envelope'></i></li>",
-  "<li>game           - Start a roguelike dungeon game <i class='fa-solid fa-dungeon'></i></li>",
-  "<li>clear          - Clear Terminal <i class='fa-solid fa-eraser'></i></li>",
-];
-
-const aboutme = [
-  "<br>",
-  "Hi there! I'm An Do, an IT enthusiast and aspiring DevOps Engineer and Fullstack Developer. ",
-  "Currently, I'm undertaking the role of IT Customer Services Officer",
-  " and a final year student at RMIT University Vietnam.",
-  " Before that, I was studying Computer Science and Data Science at UWA. ",
-  "️For the past year, I have been an active member of RMIT FinTech Club. ",
-  "Together, our club and I have successfully hosted an national competition as well as planning for another one. ",
-  "As I have NO intention to leave this field of IT anytime soon, stay tune and join me on this journey. ",
-  "</br>",
-];
-
-
-const commands = [
-  "help",
-  "aboutme",
-  "projects",
-  "linkedin",
-  "github",
-  "email",
-  "game",
-  "clear",
-];
-
-const projects = [
-  "<br>",
-  `<li><a href="" target="_blank"><u></u> <i class="fa-solid "></i></a> - </li>`,
-  `<li><a href="" target="_blank"><u></u> <i class="fa-solid "></i></a> - </li>`,
-  `<li><a href="" target="_blank"><u></u> <i class="fa-solid "></i></a> - </li>`,
-  "<br>",
-];
+import {
+  linkedin,
+  github,
+  email,
+  game,
+  help,
+  aboutme,
+  projects,
+  aboutmeByYear
+} from "./docs-cli.js";
 
 const container = document.querySelector(".container");
 const input = document.getElementById("prompt");
 let previousCmds = []; // stack ds
 let index = 1;
+let selectedYear = 2025;
 
 window.addEventListener("keydown", handleEnter);
 window.addEventListener("keydown", handleUpArrow);
@@ -142,7 +105,8 @@ function command(cmd, terminal) {
       break;
 
     case "aboutme":
-      addLine(aboutme, terminal);
+      const aboutmeText = aboutmeByYear[selectedYear] || aboutme;
+      addLine(aboutmeText, terminal);
       break;
 
     case "projects":
@@ -161,7 +125,7 @@ function command(cmd, terminal) {
 
     case "email":
       addLine(
-        `Opening mailto: <a href="${email}">noahkimCS@gmail.com</a>...`,
+        `Opening mailto: <a href="${email}">dolelongan@gmail.com</a>...`,
         terminal,
       );
       newTab(email);
